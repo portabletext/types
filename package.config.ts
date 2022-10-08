ed0f635869bfb6789456de832958c4306d6e80e7
Marius Lundgård <studio@mariuslundgard.com>
2022-10-08 12:30:13 +0200

    build: use `@sanity/pkg-utils`

---
+++ package.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from '@sanity/pkg-utils'
+
+export default defineConfig({
+  extract: {
+    rules: {
+      'ae-missing-release-tag': 'warn',
+      'tsdoc-undefined-tag': 'warn',
+    },
+  },
+  minify: false,
+})
