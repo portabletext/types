c75c119ec193cdff26df0520467e6efeeb62bfe8
Marius Lundgård <studio@mariuslundgard.com>
2023-01-27 18:29:56 +0100

    test(build): make sure ESM wrapper contains no members

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-console */
 import assert from 'node:assert'
 import * as ptTypes from '../dist/index.js'
+import esmWrapper from '../dist/index.cjs.js'
 
 assert.strictEqual(Object.keys(ptTypes).length, 0)
-console.log('✅ should have no non-type exports')
+console.log('✅ ESM bundle should have no non-type exports')
+
+assert.strictEqual(Object.keys(esmWrapper).length, 0)
+console.log('✅ ESM wrapper should have no non-type exports')
