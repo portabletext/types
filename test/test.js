6d8606f8165aec271d653886eacb812aa9c7e0a4
Cody Olsen <stipsan@gmail.com>
2023-06-23 21:23:43 +0200

    chore: fix test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 import assert from 'node:assert'
 import * as ptTypes from '../dist/index.js'
-import esmWrapper from '../dist/index.cjs.js'
+import ptTypesCjs from '../dist/index.cjs'
 
 assert.strictEqual(Object.keys(ptTypes).length, 0)
 console.log('✅ ESM bundle should have no non-type exports')
 
-assert.strictEqual(Object.keys(esmWrapper).length, 0)
+assert.strictEqual(Object.keys(ptTypesCjs).length, 0)
 console.log('✅ ESM wrapper should have no non-type exports')
