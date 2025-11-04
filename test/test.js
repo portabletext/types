681d361930218b4171ff927c08a91f2b41a6bf5e
Cody Olsen <stipsan@gmail.com>
2025-11-04 22:16:20 +0100

    no need to run lint twice

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
-import assert from 'node:assert'
+import assert from "node:assert";
 
-import ptTypesCjs from '../dist/index.cjs'
-import * as ptTypes from '../dist/index.js'
+import ptTypesCjs from "../dist/index.cjs";
+import * as ptTypes from "../dist/index.js";
 
-assert.strictEqual(Object.keys(ptTypes).length, 0)
-console.log('✅ ESM bundle should have no non-type exports')
+assert.strictEqual(Object.keys(ptTypes).length, 0);
+console.log("✅ ESM bundle should have no non-type exports");
 
-assert.strictEqual(Object.keys(ptTypesCjs).length, 0)
-console.log('✅ ESM wrapper should have no non-type exports')
+assert.strictEqual(Object.keys(ptTypesCjs).length, 0);
+console.log("✅ ESM wrapper should have no non-type exports");
